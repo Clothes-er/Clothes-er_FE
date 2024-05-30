cf0494a4fd58bd63b640c90201c92e9d547eaea2
yyypearl <hor629@naver.com>
2024-05-31 03:23:35 +0900

    docs(#8): post 더미데이터

---
+++ src/data/homeData.ts
@@ -0,0 +1,59 @@
+export interface PostList {
+    id?: number;
+    imgUrl?: string | null;
+    nickname: string;
+    title: string;
+    minPrice: number;
+    createdAt: string;
+}
+
+export const postList :PostList[] = [
+    {
+        "id": 4,
+        "imgUrl": null,
+        "nickname": "Clothes:er",
+        "title": "오프숄더 리본 반팔 티",
+        "minPrice": 4000,
+        "createdAt": "30분 전"
+    },
+    {
+        "id": 3,
+        "imgUrl": null,
+        "nickname": "Clothes:er",
+        "title": "정장 세트",
+        "minPrice": 5000,
+        "createdAt": "1시간 전"
+    },
+    {
+        "id": 2,
+        "imgUrl": null,
+        "nickname": "눈송이",
+        "title": "청바지 멜빵 원피스",
+        "minPrice": 4000,
+        "createdAt": "1일 전"
+    },
+    {
+        "id": 1,
+        "imgUrl": null,
+        "nickname": "Clothes:er",
+        "title": "오프숄더 리본 반팔 티",
+        "minPrice": 4000,
+        "createdAt": "30분 전"
+    },
+    {
+        "id": 5,
+        "imgUrl": null,
+        "nickname": "Clothes:er",
+        "title": "정장 세트",
+        "minPrice": 5000,
+        "createdAt": "1시간 전"
+    },
+    {
+        "id": 6,
+        "imgUrl": null,
+        "nickname": "눈송이",
+        "title": "청바지 멜빵 원피스",
+        "minPrice": 4000,
+        "createdAt": "1일 전"
+    },
+];
