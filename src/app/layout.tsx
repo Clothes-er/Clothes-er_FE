89a13cb97e59eec4e8cb767dc3d07ee8ffe5d4e4
yyypearl <hor629@naver.com>
2024-06-06 14:29:35 +0900

    style(#8): next.js Styled-component 적용 (registry)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import GlobalStyles from "@/styles/GlobalStyle";
 import NOSSR from "@/util/NOSSR";
 import { Provider } from "react-redux";
 import { store } from "@/redux/store";
+import StyledJsxRegistry from "./registry";
 
 export default function RootLayout({
   children,
@@ -18,12 +19,14 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body>
-        <Provider store={store}>
-          <ThemeProvider theme={theme}>
-            <GlobalStyles />
-            <NOSSR>{children}</NOSSR>
-          </ThemeProvider>
-        </Provider>
+        <StyledJsxRegistry>
+          <Provider store={store}>
+            <ThemeProvider theme={theme}>
+              <GlobalStyles />
+              <NOSSR>{children}</NOSSR>
+            </ThemeProvider>
+          </Provider>
+        </StyledJsxRegistry>
       </body>
     </html>
   );
