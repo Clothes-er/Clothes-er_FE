e73897fbac90ee40f3a56dc3d4f89cdf48422a93
yyypearl <hor629@naver.com>
2024-06-07 15:25:04 +0900

    feat(#12): Gender Enum 설정

---
+++ src/interface/Gender.tsx
@@ -0,0 +1,16 @@
+export enum Gender {
+  MALE = "MALE",
+  FEMALE = "FEMALE",
+}
+
+// 함수 정의: enum 값에 따라 문자열 반환
+export const getGenderLabel = (gender: Gender): string => {
+  switch (gender) {
+    case Gender.MALE:
+      return "남자";
+    case Gender.FEMALE:
+      return "여자";
+    default:
+      return "";
+  }
+};
