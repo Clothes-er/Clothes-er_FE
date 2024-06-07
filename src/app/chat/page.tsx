88db7a770623180feaf95fd3f1218794b6eac5a3
yyypearl <hor629@naver.com>
2024-06-07 15:58:53 +0900

    feat(#12): 채팅 목록 UI 및 API 연동

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,11 +1,70 @@
 "use client";
+import AuthAxios from "@/api/authAxios";
+import ChatPreview from "@/components/chat/ChatPreview";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 
+interface ChatList {
+  id: number;
+  nickname: string;
+  profileImgUrl: string;
+  recentMessage: string;
+  recentMessageTime: string;
+  recentImgUrl: string;
+  recentState: string;
+  title: string;
+}
 const Chat = () => {
+  const router = useRouter();
+  const [chatList, setChatList] = useState<ChatList[]>();
+
+  useEffect(() => {
+    AuthAxios.get("/api/v1/chats/rooms")
+      .then((response) => {
+        const data = response.data.result;
+        setChatList(data);
+        console.log(data);
+        console.log(response.data.message);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
-    <>
-      <Layout>채팅</Layout>
-    </>
+    <Layout>
+      <Image
+        src="/assets/images/logo_black.svg"
+        width={101}
+        height={18}
+        alt="logo"
+        onClick={() => router.push("/home")}
+        style={{ cursor: "pointer" }}
+      />
+      <Top>
+        <Image
+          src="/assets/icons/ic_arrow.svg"
+          width={24}
+          height={24}
+          alt="back"
+          onClick={() => router.back()}
+          style={{ cursor: "pointer" }}
+        />
+        채팅
+      </Top>
+      <ChatList>
+        {chatList?.map((data) => (
+          <ChatPreview
+            key={data.id}
+            nickname={data.nickname}
+            recentMessage={data.recentMessage}
+            title={data.title}
+          />
+        ))}
+      </ChatList>
+    </Layout>
   );
 };
 
@@ -21,4 +80,19 @@ const Layout = styled.div`
   flex-direction: column;
   gap: 23px;
   position: relative;
+`;
+
+const Top = styled.div`
+  width: calc(50% + 30px);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 18px;
+  margin-bottom: 15px;
+  ${(props) => props.theme.fonts.h2_bold};
+`;
+
+const ChatList = styled.div`
+  display: flex;
+  flex-direction: column;
 `;
