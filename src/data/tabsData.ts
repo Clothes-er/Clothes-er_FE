9915d626bc30eb9ec72419b4be19fb8ef4b77980
yyypearl <hor629@naver.com>
2024-06-09 02:49:46 +0900

    feat: Tabbar 수정

---
+++ src/data/tabsData.ts
@@ -0,0 +1,5 @@
+export const tabs = [
+  { path: "/home", icon: "ic_home", label: "홈" },
+  { path: "/chat", icon: "ic_chat", label: "채팅" },
+  { path: "/mycloset", icon: "ic_my", label: "나의 옷장" },
+];
