6c08a628fcd8b89a905d5d5e80a886d4bb14983d
yyypearl <hor629@naver.com>
2024-06-14 00:03:38 +0900

    chore(#19): s3 이미지 허용

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,21 @@ const nextConfig = {
         hostname: "clotheser-s3-bucket.s3.ap-northeast-2.amazonaws.com",
         pathname: "/rentals/**",
       },
+      {
+        protocol: "https",
+        hostname: "clotheser-s3-bucket.s3.ap-northeast-2.amazonaws.com",
+        pathname: "/profiles/**",
+      },
+      {
+        protocol: "https",
+        hostname: "clotheser-s3-bucket.s3.ap-northeast-2.amazonaws.com",
+        pathname: "/users/**",
+      },
+      {
+        protocol: "https",
+        hostname: "clotheser-s3-bucket.s3.ap-northeast-2.amazonaws.com",
+        pathname: "/chat/**",
+      },
     ],
   },
 };
