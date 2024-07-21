029ec5612b8350992c964e75b445069b21aeffac
yyypearl <hor629@naver.com>
2024-07-22 00:51:50 +0900

    feat(#37): 프로필 수정 API 연동

--- src/app/mycloset/profile/page.tsx
+++ src/app/mycloset/profile/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 import AuthAxios from "@/api/authAxios";
+import Axios from "@/api/axios";
 import Input from "@/components/common/Input";
-import Tabbar from "@/components/common/Tabbar";
+import { getToken } from "@/hooks/getToken";
 import { useRequireAuth } from "@/hooks/useAuth";
 import { theme } from "@/styles/theme";
+import axios from "axios";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
@@ -24,15 +26,20 @@ const UserInfo = () => {
   const router = useRouter();
   const [userInfo, setUserInfo] = useState<UserInfo>();
 
+  const [image, setImage] = useState<File | null>(null);
   /* 닉네임 */
   const [nickname, setNickname] = useState<string>("");
+  const [successMsg, setSuccessMsg] = useState<string>("");
   const [errorMsg, setErrorMsg] = useState<string>("");
+
+  const isModify = (image || userInfo?.nickname !== nickname) && !errorMsg;
 
   useEffect(() => {
     AuthAxios.get("/api/v1/users/info")
       .then((response) => {
         const data = response.data.result;
         setUserInfo(data);
+        setNickname(data.nickname);
         console.log(data);
         console.log(response.data.message);
       })
@@ -40,6 +47,71 @@ const UserInfo = () => {
         console.log(error);
       });
   }, []);
+
+  /* 닉네임 중복확인 */
+  useEffect(() => {
+    if (userInfo?.nickname !== nickname && nickname !== "") {
+      Axios.get(`/api/v1/users/check-nickname/${nickname}`)
+        .then((response) => {
+          console.log("닉네임 중복확인 성공", response.data);
+          setSuccessMsg("사용가능한 닉네임입니다.");
+        })
+        .catch((error) => {
+          if (error.response && error.response.status === 409) {
+            setSuccessMsg("");
+            setErrorMsg(error.response.data.message);
+          } else {
+            console.log("닉네임 중복확인 실패", error);
+          }
+        });
+    } else if (userInfo?.nickname === nickname) {
+      setSuccessMsg("");
+      setErrorMsg("");
+    }
+  }, [nickname, userInfo]);
+
+  const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files && files.length > 0) {
+      setImage(files[0]);
+    }
+  };
+
+  const handleModifyProfile = () => {
+    const formData = new FormData();
+
+    formData.append(
+      "patch",
+      new Blob(
+        [
+          JSON.stringify({
+            nickname: nickname,
+          }),
+        ],
+        { type: "application/json" }
+      )
+    );
+
+    if (image) {
+      formData.append("image", image);
+    }
+
+    axios
+      .patch("/api/v1/users/profile", formData, {
+        baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+        headers: {
+          Authorization: `Bearer ${getToken()}`,
+        },
+      })
+      .then((response) => {
+        console.log(response.data.result);
+        router.push(`/mycloset`);
+      })
+      .catch((error) => {
+        setErrorMsg(error.response.data.message);
+        console.log(error.response.data.message);
+      });
+  };
 
   return (
     <>
@@ -62,25 +134,41 @@ const UserInfo = () => {
             style={{ cursor: "pointer" }}
           />
           프로필 수정
-          <SubmitButton>완료</SubmitButton>
+          <SubmitButton onClick={handleModifyProfile} disabled={!isModify}>
+            완료
+          </SubmitButton>
         </Top>
         <Content>
           <Profile>
             {userInfo?.profileUrl && (
               <Image
-                src={userInfo.profileUrl || "/assets/images/profile.svg"}
+                src={
+                  image
+                    ? URL.createObjectURL(image)
+                    : userInfo?.profileUrl || "/assets/images/profile.svg"
+                }
                 width={147}
                 height={147}
                 alt="profile"
                 style={{ borderRadius: "100px", background: "white" }}
               />
             )}
-            <EditImage
-              src={"/assets/icons/ic_circle_edit.svg"}
-              width={50}
-              height={50}
-              alt="edit"
-            />
+            <ModifyPhoto>
+              <input
+                type="file"
+                accept="image/*"
+                multiple
+                onChange={handleFileUpload}
+                style={{ display: "none" }}
+              />
+              <Image
+                src={"/assets/icons/ic_camera_edit.svg"}
+                width={30}
+                height={30}
+                alt="edit"
+                onClick={() => document.getElementById("fileInput")?.click()}
+              />
+            </ModifyPhoto>
           </Profile>
           <Info>
             <Input
@@ -88,7 +176,8 @@ const UserInfo = () => {
               label="닉네임"
               value={nickname}
               size="small"
-              placeholder="진도리"
+              placeholder="닉네임"
+              successMsg={successMsg}
               errorMsg={errorMsg}
               onChange={(value: string) => {
                 setNickname(value);
@@ -125,10 +214,14 @@ const Top = styled.div`
 `;
 
 const SubmitButton = styled.button`
-  color: ${theme.colors.gray800};
+  color: ${theme.colors.purple800};
   ${(props) => props.theme.fonts.h2_bold};
   border: none;
   background: none;
+
+  &:disabled {
+    color: ${theme.colors.gray800};
+  }
 `;
 
 const Content = styled.div`
@@ -148,11 +241,18 @@ const Profile = styled.div`
   position: relative;
 `;
 
-const EditImage = styled(Image)`
+const ModifyPhoto = styled.label`
+  width: 30px;
+  height: 30px;
+  border-radius: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   position: absolute;
-  bottom: -5px;
-  right: -5px;
-  filter: drop-shadow(0px 4px 15px rgba(85, 85, 85, 0.25));
+  bottom: 8px;
+  right: 8px;
+  filter: drop-shadow(0px 4px 20px rgba(85, 85, 85, 0.25));
+  cursor: pointer;
 `;
 
 const Info = styled.div`
