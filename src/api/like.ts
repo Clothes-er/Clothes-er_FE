12954830b39a8e10edb46381affc1bded541be9c
yyypearl <hor629@naver.com>
2024-10-11 06:12:13 +0900

    feat(#100): 대여옷 찜하기 목록 기능

--- src/api/like.ts
+++ src/api/like.ts
@@ -1,6 +1,6 @@
 import AuthAxios from "./authAxios"
 
-export const postHomeLike = async(rentalId: number) => {
+export const postRentalLike = async(rentalId: number) => {
    try {
     const response = await AuthAxios.post(`/api/v1/rentals/${rentalId}/like`);
     console.log("대여글 찜 생성 성공");
@@ -11,13 +11,24 @@ export const postHomeLike = async(rentalId: number) => {
    }
 }
 
-export const deleteHomeLike = async(rentalId: number) => {
+export const deleteRentalLike = async(rentalId: number) => {
    try {
     const response = await AuthAxios.delete(`/api/v1/rentals/${rentalId}/like`);
     console.log("대여글 찜 삭제 성공");
     return response.data;
 } catch (error){
        console.log("대여글 찜 삭제 실패");
+    throw error;
+   }
+}
+
+export const getRentalLikes = async() => {
+   try {
+    const response = await AuthAxios.get(`/api/v1/closet/like-rentals`);
+    console.log("나의 대여글 찜 목록 조회 성공");
+    return response.data;
+} catch (error){
+       console.log("나의 대여글 찜 목록 조회 실패");
     throw error;
    }
 }
