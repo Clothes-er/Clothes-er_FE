1301d86e432f6b807137c0df164731588c1f3e9a
yyypearl <hor629@naver.com>
2024-10-16 17:14:36 +0900

    feat(#100): 보유옷 찜 관련 API 정의

--- src/api/like.ts
+++ src/api/like.ts
@@ -32,3 +32,38 @@ export const getRentalLikes = async() => {
     throw error;
    }
 }
+
+export const postClothesLike = async (clothesId: number) => {
+  try {
+    const response = await AuthAxios.post(`/api/v1/clothes/${clothesId}/like`);
+    console.log("보유글 찜 생성 성공");
+    return response.data;
+  } catch (error) {
+    console.log("대여글 찜 생성 실패");
+    throw error;
+  }
+};
+
+export const deleteClothesLike = async (clothesId: number) => {
+  try {
+    const response = await AuthAxios.delete(
+      `/api/v1/clothes/${clothesId}/like`
+    );
+    console.log("보유글 찜 삭제 성공");
+    return response.data;
+  } catch (error) {
+    console.log("대여글 찜 삭제 실패");
+    throw error;
+  }
+};
+
+export const getClothesLikes = async () => {
+  try {
+    const response = await AuthAxios.get(`/api/v1/closet/like-clothes`);
+    console.log("나의 보유글 찜 목록 조회 성공");
+    return response.data;
+  } catch (error) {
+    console.log("나의 보유글 찜 목록 조회 실패");
+    throw error;
+  }
+};
