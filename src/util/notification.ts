2beb739a41ebb03cff667fb8d8eb52479ca47d5f
yyypearl <hor629@naver.com>
2024-10-27 22:37:24 +0900

    fix(#118): 서버 환경에서 requestPermission 실행되지 않게 수정

--- src/util/notification.ts
+++ src/util/notification.ts
@@ -1,20 +1,19 @@
 import { getDeviceToken, patchDeviceToken } from "@/api/deviceToken";
 
-export function registerServiceWorker() {
-    if (typeof window !== "undefined" && "serviceWorker" in navigator) {
-      navigator.serviceWorker
-        .register("firebase-messaging-sw.js")
-        .then(function (registration) {
-          console.log("Service Worker 등록 성공:", registration);
-        })
-        .catch(function (error) {
-          console.log("Service Worker 등록 실패:", error);
-        });
+export async function registerServiceWorker() {
+  if (typeof window !== "undefined" && "serviceWorker" in navigator) {
+    try {
+      const registration = await navigator.serviceWorker.register("firebase-messaging-sw.js");
+      console.log("Service Worker 등록 성공:", registration);
+    } catch (error) {
+      console.log("Service Worker 등록 실패:", error);
     }
+  }
 }
 
 export async function handleAllowNotification() {
-  if (typeof window !== "undefined") {
+  if (typeof window === "undefined") return;
+  
     const permission = await Notification.requestPermission();
     if (permission === "granted") {
       console.log("알림 권한이 허용되었습니다.");
@@ -29,6 +28,6 @@ export async function handleAllowNotification() {
       console.log("알림 권한이 거부되었습니다.");
     } else {
       console.log("사용자가 알림 권한을 결정하지 않았습니다.");
-    }
   }
+    return permission;
 }
