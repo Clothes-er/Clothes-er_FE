5bbec4f0a001867845e3378a64b020eefcf4b270
yyypearl <hor629@naver.com>
2024-11-10 20:01:29 +0900

    feat(#130): 유저 프로필 팔로우 관련 정보 및 버튼 추가

---
+++ src/app/user/[userSid]/follow/page.tsx
@@ -0,0 +1,38 @@
+// "use client";
+
+// import Header from "@/components/common/Header";
+// import ListTab from "@/components/common/ListTab";
+// import Topbar from "@/components/common/Topbar";
+// import { theme } from "@/styles/theme";
+// import { useParams } from "next/navigation";
+// import React from "react";
+// import styled from "styled-components";
+
+// const UserFollowPage = () => {
+//   const { userSid } = useParams();
+
+//   return (
+//     <>
+//       <Layout>
+//         <Header />
+//         <Topbar text="유저명" icon={true} align="center" />
+//         <ListTab listType="follow" />
+//       </Layout>
+//     </>
+//   );
+// };
+
+// export default UserFollowPage;
+
+// const Layout = styled.div`
+//   width: 100%;
+//   height: 100%;
+//   overflow-y: scroll;
+//   padding: 42px 20px;
+//   display: flex;
+//   flex-direction: column;
+//   position: relative;
+//   overflow-x: none;
+//   background: ${theme.colors.ivory};
+//   z-index: 1;
+// `;
